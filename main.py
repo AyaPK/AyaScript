f4720985c5c363b54cbcd82fc615b25149298d2b
Aya <47668950+AyaPK@users.noreply.github.com>
2020-05-27 21:56:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from ayascript import ayascript
 
-funcs = {
-
+nodes = {
+    # the output key will be where you can create sentence structures to be created
+    # surround a word with hashes to turn it in to a node
     "output": {
         "#word1.cap# #word2#, Aya!",
         "It's #adjective.a# #noun#"
     },
 
+    # keys following the output node will contain values that are
+    # randomly selected when generating the final sentence
     "word1": {
-        "cool","neat","weird","interesting"
+        "cool", "neat", "weird", "interesting"
     },
 
     "word2": {
@@ -16,14 +19,15 @@ funcs = {
     },
 
     "noun": {
-        "cat","dog","fish","mouse","pet","house"
+        "cat", "dog", "fish", "mouse", "pet", "house"
     },
 
     "adjective": {
-        "quiet","big","small","red","blue","awesome","angry","empty"
+        "quiet", "big", "small", "red", "blue", "awesome", "angry", "empty"
     }
 
 }
 
-output = ayascript(funcs)
+# call the ayascript function and pass in your nodes dictionary to generate a sentence
+output = ayascript(nodes)
 print(output)
